5ea6d3d1306c87b05df92e0d862d7cff8e0c793c
NamigGuluzadeh <nquluzade95@gmail.com>
2023-08-16 13:27:50 +0400

    Initial commit

---
+++ src/main/java/com/example/jwtsecurity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.jwtsecurity.repository;
+
+import com.example.jwtsecurity.user.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+
+    Optional<User> findByEmail(String email);
+}
